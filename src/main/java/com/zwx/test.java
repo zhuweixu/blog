b16408a62888f8b0b6249c9d6e3883ad4c2900ae
HZHao682 <greywon@qq.com>
2020-07-02 16:47:50 +0800

    branch test

--- src/main/java/com/zwx/test.java
+++ src/main/java/com/zwx/test.java
@@ -1,20 +1,31 @@
 package com.zwx;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class test {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        if (num1>=num2&&num1>=num3){
-            System.out.println("最大的数为："+num1);
-        }else if (num2>=num1&&num2>=num3){
-            System.out.println("最大的数为："+num2);
-        }else {
-            System.out.println("最大的数为："+num3);
+        Integer[] aele = new Integer[]{1,2,3,4};
+        Integer[] bele = new Integer[]{4,5,6,7,0};
+        System.out.println(getSameElement(aele,bele));
+    }
+
+    static Set<Integer> getSameElement(Integer[] aEle, Integer[] bEle) {
+        //sameElement：最后返回的相同的元素
+        Set<Integer> sameElement = new HashSet<>();
+        //tempElement：临时存放的元素（aEle or bEle）
+        Set<Integer> tempElement = new HashSet<>();
+        //将aEle放到集合中
+        Collections.addAll(tempElement, aEle);
+        //判断bEle中的元素是否存在于tempElement
+        for (Integer ele : bEle) {
+            if (tempElement.contains(ele)) {
+                sameElement.add(ele);
+            }
         }
+        return sameElement;
     }
 }
 
